81adb33bd3d5647088d1a4cfe238fe0068b25ca3
Chadwick Seth W <seth.chadwick@gmail.com>
2021-03-11 16:36:48 -0600

    added packages for authentication (JWT) but have not implemented it in the app at this time

--- server.js
+++ server.js
@@ -2,7 +2,33 @@ const express = require("express");
 const app = express();
 const methodOverride = require("method-override");
 
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const createError = require("http-errors");
 
+
+// Token Verification
+const verifyToken = (req, res, next) => {
+  let token = req.cookies.jwt;
+  // COOKIE PARSER GIVES YOU A .cookies PROP, WE NAMED OUR TOKEN jwt
+  console.log("Cookies: ", req.cookies.jwt);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
+    if (err || !decodedUser) {
+      return res.status(401).json({ error: "Unauthorized Request" });
+    }
+    req.user = decodedUser;
+    // ADDS A .user PROP TO REQ FOR TOKEN USER
+    console.log(decodedUser);
+
+    next();
+  });
+};
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
 app.use(methodOverride("_method"));
 
 
@@ -13,6 +39,7 @@ app.get("/", (req, res) => {
     array: ["Seth", "Chadwick"],
   });
 });
+
 
 // app.use("/api/auth", require("./controllers/authController.js"));
 // app.use("/api/users", require("./controllers/usersController.js"));
