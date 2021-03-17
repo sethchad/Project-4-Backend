ca2103061d1de45e7bc99acbe9379489af13c0f0
Chadwick Seth W <seth.chadwick@gmail.com>
2021-03-17 13:55:45 -0500

    added a third meal with ingredients to the seeder file

--- seeders/20210312144537-demo-user-meal-ingredient.js
+++ seeders/20210312144537-demo-user-meal-ingredient.js
@@ -17,6 +17,12 @@ module.exports = {
         location: "Moline, IL",
         directions: "1. Preheat the oven to 230°C.\r\n\r\n2. Add the sugar and crumble the fresh yeast into warm water.\r\n\r\n3. Allow the mixture to stand for 10 – 15 minutes in a warm place (we find a windowsill on a sunny day works best) until froth develops on the surface.\r\n\r\n4. Sift the flour and salt into a large mixing bowl, make a well in the middle and pour in the yeast mixture and olive oil.\r\n\r\n5. Lightly flour your hands, and slowly mix the ingredients together until they bind.\r\n\r\n6. Generously dust your surface with flour.\r\n\r\n7. Throw down the dough and begin kneading for 10 minutes until smooth, silky and soft.\r\n\r\n8. Place in a lightly oiled, non-stick baking tray (we use a round one, but any shape will do!)\r\n\r\n9. Spread the passata on top making sure you go to the edge.\r\n\r\n10. Evenly place the mozzarella (or other cheese) on top, season with the oregano and black pepper, then drizzle with a little olive oil.\r\n\r\n11. Cook in the oven for 10 – 12 minutes until the cheese slightly colours.\r\n\r\n12. When ready, place the basil leaf on top and tuck in!"
       },
+      { 
+        name: "Beef Dumpling Stew", 
+        imageUrl: "https://www.themealdb.com/images/media/meals/uyqrrv1511553350.jpg", 
+        location: "Chicago, IL",
+        directions: "Preheat the oven to 180C/350F/Gas 4.\r\n\r\nFor the beef stew, heat the oil and butter in an ovenproof casserole and fry the beef until browned on all sides.\r\n\r\nSprinkle over the flour and cook for a further 2-3 minutes.\r\n\r\nAdd the garlic and all the vegetables and fry for 1-2 minutes.\r\n\r\nStir in the wine, stock and herbs, then add the Worcestershire sauce and balsamic vinegar, to taste. Season with salt and freshly ground black pepper.\r\n\r\nCover with a lid, transfer to the oven and cook for about two hours, or until the meat is tender.\r\n\r\nFor the dumplings, sift the flour, baking powder and salt into a bowl.\r\nAdd the suet and enough water to form a thick dough.\r\n\r\nWith floured hands, roll spoonfuls of the dough into small balls.\r\n\r\nAfter two hours, remove the lid from the stew and place the balls on top of the stew. Cover, return to the oven and cook for a further 20 minutes, or until the dumplings have swollen and are tender. (If you prefer your dumplings with a golden top, leave the lid off when returning to the oven.)\r\n\r\nTo serve, place a spoonful of mashed potato onto each of four serving plates and top with the stew and dumplings. Sprinkle with chopped parsley."
+      },
     ]);
 
     let ingredients = await queryInterface.bulkInsert("Ingredients", [
@@ -33,6 +39,13 @@ module.exports = {
       { description: "1 1/2 tsp salt", mealId: 2 },
       { description: "1 cup mozzarella", mealId: 2 },
       { description: "1 sliced tomato", mealId: 2 },
+      { description: "1 lb beef", mealId: 3 },
+      { description: "1 lb carrots", mealId: 3 },
+      { description: "1 Tbsp tyme", mealId: 3 },
+      { description: "1 lb onions", mealId: 3 },
+      { description: "4 cups beef stock", mealId: 3 },
+      { description: "2 cups water", mealId: 3 },
+      { description: "1 lb of potatoes", mealId: 3 },
     ]);
 
     let users = await queryInterface.bulkInsert("Users", [
