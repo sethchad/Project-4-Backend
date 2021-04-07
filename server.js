13e75ea16804830ebc1d442a887989072e026514
Chadwick Seth W <seth.chadwick@gmail.com>
2021-04-07 10:33:01 -0500

    Removed commented out code

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const app = express();
 const methodOverride = require("method-override");
-
-// const jwt = require("jsonwebtoken");
-// const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const createError = require("http-errors");
 
-
+// MIDDLEWARE
 app.use(cors());
-// app.use(cookieParser());
 app.use(express.json());
 app.use(methodOverride("_method"));
 
 
-// HOMEPAGE
+// HOMEPAGE ROUTE
 app.get("/", (req, res) => {
   res.json({ 
     message: "Recipe Book API is working",
@@ -22,8 +17,9 @@ app.get("/", (req, res) => {
   });
 });
 
-
+// CONTROLLER AND ROUTES
 app.use("/api/meals", require("./controllers/mealsController.js"));
+
 
 app.listen(process.env.PORT, () => {
   console.log("Nodemon listening");
